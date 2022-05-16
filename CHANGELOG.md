6efb06e0b1e0c170f30daa46458a0bf631aeb0e1
brainfoolong <brainfoolong@users.noreply.github.com>
2022-05-16 17:18:54 +0200

    fixed issue on first time install and nginx starts before module is available

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## [1.0.2]
+
+* fixed issue on first time install and nginx starts before module is available
+
 ## [1.0.1]
 
 * fixed bugs on first time install (tar unpack and not empty db)
--- config/nginx-run.sh
+++ config/nginx-run.sh
@@ -1,5 +1,11 @@
 . /opt/bitnami/scripts/liblog.sh
 cp /opt/bitnami/nginx/nginx-config-template.conf /opt/bitnami/nginx/conf/server_blocks/default.conf
+
+until [ -f /framelix/MODULE ]; do
+  echo "Waiting for phpfpm to create app/MODULE out of app release files"
+  sleep 1
+done
+
 FRAMELIX_MODULE=$(cat /framelix/MODULE)
 info "** Injecting FRAMELIX_MODULE->$FRAMELIX_MODULE into /opt/bitnami/nginx/conf/server_blocks/default.conf **"
 sed -i "s/{module}/$FRAMELIX_MODULE/g" /opt/bitnami/nginx/conf/server_blocks/default.conf
